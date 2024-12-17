7572945301bb9701026da3d6b0e42ccc48001f67
FernandoTejedorL <fernandotejedorlopez@gmail.com>
2024-12-17 14:17:47 +0100

    Initial commit

---
+++ src/styles/GlobalStyles.js
@@ -0,0 +1,34 @@
+import { createGlobalStyle } from 'styled-components';
+import './normalize.css';
+import { FONT_WEIGHT } from './fonts';
+import { COLORS } from './colors';
+export const GlobalStyles = createGlobalStyle`
+    *,
+    *::after,
+    *::before {
+        box-sizing: border-box;
+    }
+
+    img {
+        max-width: 100%;
+        display: block;
+    }
+
+    ul {
+        list-style: none;
+        padding-left: 0;
+        margin-top: 0;
+        margin-bottom: 0;
+    }
+
+    a {
+        text-decoration: none;
+        color: inherit;
+    }
+
+    body{
+        font-weight: ${FONT_WEIGHT.regular};
+        background-color: ${COLORS.white};
+    }
+
+`;

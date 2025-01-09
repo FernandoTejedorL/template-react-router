8d90d41225fccd1fd041dc39b1b666a2077e4988
FernandoTejedorL <fernandotejedorlopez@gmail.com>
2025-01-09 12:15:05 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,26 @@
 import { BrowserRouter, Link } from 'react-router-dom';
 import Router from './router/Router';
 import { GlobalStyles } from './styles/GlobalStyles';
-import TemplateProvider from './providers/TemplateProvider';
 
 const App = () => {
 	return (
 		<>
 			<GlobalStyles />
-			<TemplateProvider>
-				<BrowserRouter>
-					<nav>
-						<ul>
-							<Link to='/'>Home</Link>
-						</ul>
-						<ul>
-							<Link to='/about'>About</Link>
-						</ul>
-					</nav>
-					<button>One</button>
-					<button>Two</button>
-					<button>Three</button>
-					<Router />
-				</BrowserRouter>
-			</TemplateProvider>
+
+			<BrowserRouter>
+				<nav>
+					<ul>
+						<Link to='/'>Home</Link>
+					</ul>
+					<ul>
+						<Link to='/about'>About</Link>
+					</ul>
+				</nav>
+				<button>One</button>
+				<button>Two</button>
+				<button>Three</button>
+				<Router />
+			</BrowserRouter>
 		</>
 	);
 };
